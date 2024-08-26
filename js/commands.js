c7203b1fadfe051a3ca2841125784231846def8f
shkev03 <keval.jignesh.shah@gmail.com>
2024-08-26 15:55:06 +0530

    V2 Commands Added

--- js/commands.js
+++ js/commands.js
@@ -1,3 +1,86 @@
-const commands = [
-    
+const ls = [
+    `
+        <div class='list'>
+            <p>
+                <b>projects/</b>
+            </p>
+            <p>about.me</p>
+            <p>contact.me</p>
+        <div>
+    `,
+];
+
+const ls_projects = [
+    `
+        <div class='list'>
+            <a target="_blank" href='https://amazon-gray.vercel.app/'>> Amazon-Clone/</a>
+            <a target="_blank" href='https://github.com/ShKev03/WhatsChat'>> Whatsapp-Clone/</a>
+            <a target="_blank" href='https://github.com/ShKev03/Instagram'>> Instagram-Clone/</a>
+            <a target="_blank" href='https://github.com/ShKev03/Zoom'>> Zoom-Clone/</a>
+            <a target="_blank" href='https://github.com/ShKev03/Facebook'>> Facebook-Clone/</a>
+        </div>
+    `,
+];
+
+const invalid = [
+    `<p>INVALID COMMAND :</p>
+        <p>
+            Please enter a valid command or enter 
+            <b>'help'</b> 
+            command to check out the commands available.
+        </p>
+    `,
+];
+
+const help = [
+    `   
+        <div class="comment"># available commands (basic)</div>
+        <pre>ls             lists all available files and directories</pre>
+        <pre>cd             changes the current directory</pre>
+        <pre>pwd            prints the current working directory</pre>
+        <pre>clear          clears the terminal output</pre>
+        <pre>cat            reads the files and prints them</pre>
+        <pre>whoami         prints more about me </pre>
+        <pre>help           prints all available commands </pre>
+        <pre class="hidden">_</pre>
+        <div class="comment"># additional commands (to contact me)</div>
+        <pre>gmail          opens gmail in new tab</pre>
+        <pre>email          opens default email client</pre>
+        <pre>instagram      opens my instagram page in new tab</pre>
+        <pre>github         opens my github page in new tab</pre>
+        <pre>x              opens my x.com page in new tab</pre>
+
+    `,
+];
+
+const introduction = [
+    `
+        <div class="hidden">_</div>
+        <p class="introduction">
+            I am <strong>Keval Shah</strong>.
+        </p>
+        <div class="hidden">_</div>
+        <p class="output animated-output">
+            Passionate about <b>Software development</b> and
+            <b>cybersecurity</b>, I am constantly driven to
+            expand my knowledge in creating innovative Apps
+            and understanding the intricasis of hacking.
+            With a Strong foundation in coding and keen eye
+            for detail, I thrive in environments that
+            challenge me to solve problems and enhance
+            security measures.
+        </p>
+        <p class="output animated-output-delayed">
+            <b>My goal</b> is to contribute to projects that
+            push the boundries of technology while ensuring
+            robust and secure user experiences. I am always
+            eager to learn new technologies and
+            methodologies, and I enjoy collaborating with
+            others to bring creative solutions to my life.
+            My commitment to continuous improvement keeps me
+            motivated to stay at the forefront of the
+            industry advancement.
+        </p>
+        <div class="hidden">_</div>
+    `,
 ];
--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ function focusInput() {
     document.getElementById("command").focus();
 }
 
-const prompt = "<strong>$</strong>";
+let prompt = "<strong>keval@portfolio:/~$</strong>";
 
 let output = document.querySelector(".output");
 
@@ -15,23 +15,307 @@ command.addEventListener("keydown", (e) => {
         const input = command.value;
         output.innerHTML += `<p> ${prompt} ${input}</p>`;
         commander(input);
+        command.value = "";
     }
 });
+
+let directory = "/";
 
 function commander(cmd) {
     switch (cmd.toLowerCase()) {
         case "":
-            break
-        case "ls":
-            output.innerHTML +=
-                "this command shows the directory in this folder";
             break;
+
         case "clear":
             output.innerHTML = "";
+            break;
+
+        case "help":
+            processor(help);
+            break;
+
+        case "ls":
+        case "ls .":
+        case "ls ./":
+            if (directory == "/") {
+                processor(ls);
+            } else {
+                processor(ls_projects);
+            }
+            break;
+
+        case "ls projects":
+        case "ls /projects":
+        case "ls ./projects":
+        case "ls projects/":
+        case "ls /projects/":
+        case "ls ./projects/":
+            if (directory == "/") {
+                processor(ls_projects);
+            } else {
+                processor("Directory not found: " + directory);
+            }
+            break;
+
+        case "cd /":
+            directory = "/";
+            processor(`<p>Current directory: ${directory}</p>`);
+            prompt.innerHTML = `<a>[${directory}]</a>${prompt}`;
+            break;
+
+        case "cd projects":
+        case "cd /projects":
+        case "cd ./projects":
+        case "cd projects/":
+        case "cd /projects/":
+        case "cd ./projects/":
+            directory = "/projects";
+            processor(`<p>Current directory: ${directory}</p>`);
+            prompt.innerHTML = `<a>[${directory}]</a>${prompt}`;
+            break;
+
+        case "cd ../":
+        case "cd ..":
+            if (directory == "/") {
+                processor("Access Denied ...");
+            } else {
+                directory = "/";
+            }
+            break;
+
+        case "pwd":
+            processor(`<p>Current directory: ${directory}</p>`);
+            break;
+
+        case "cd ../..":
+        case "cd ../../":
+            processor("Access Denied ...");
+            break;
+
+        case "cd /amazon-clone":
+        case "cd amazon-clone":
+        case "cd ./amazon-clone":
+        case "cd /amazon-clone/":
+        case "cd amazon-clone/":
+        case "cd ./amazon-clone/":
+            if (directory == "/projects") {
+                processor("opening /Amazon-Clone on github.com ...");
+                processor(
+                    "redirecting to https://github.com/ShKev03/Amazon..."
+                );
+                window
+                    .open("https://github.com/ShKev03/Amazon", "_blank")
+                    .focus();
+            } else {
+                processor("Directory not found: ./Amazon-Clone");
+            }
+            break;
+
+        case "cd projects/amazon-clone":
+        case "cd /projects/amazon-clone":
+        case "cd ./projects/amazon-clone":
+        case "cd projects/amazon-clone/":
+        case "cd /projects/amazon-clone/":
+        case "cd ./projects/amazon-clone/":
+            processor("opening /Amazon-Clone on github.com ...");
+            processor("redirecting to https://github.com/ShKev03/Amazon ...");
+            window.open("https://github.com/ShKev03/Amazon", "_blank").focus();
+            break;
+
+        case "cd /zoom-clone":
+        case "cd zoom-clone":
+        case "cd ./zoom-clone":
+        case "cd /zoom-clone/":
+        case "cd zoom-clone/":
+        case "cd ./zoom-clone/":
+            if (directory == "/projects") {
+                processor("opening /Zoom-Clone on github.com ...");
+                processor("redirecting to https://github.com/ShKev03/Zoom...");
+                window
+                    .open("https://github.com/ShKev03/Zoom", "_blank")
+                    .focus();
+            } else {
+                processor("Directory not found: ./Zoom-Clone");
+            }
+            break;
+
+        case "cd projects/zoom-clone":
+        case "cd /projects/zoom-clone":
+        case "cd ./projects/zoom-clone":
+        case "cd projects/zoom-clone/":
+        case "cd /projects/zoom-clone/":
+        case "cd ./projects/zoom-clone/":
+            processor("opening /Zoom-Clone on github.com ...");
+            processor("redirecting to https://github.com/ShKev03/Zoom ...");
+            window.open("https://github.com/ShKev03/Zoom", "_blank").focus();
+            break;
+
+        case "cd /whatsapp-clone":
+        case "cd whatsapp-clone":
+        case "cd ./whatsapp-clone":
+        case "cd /whatsapp-clone/":
+        case "cd whatsapp-clone/":
+        case "cd ./whatsapp-clone/":
+            if (directory == "/projects") {
+                processor("opening /Whatsapp-Clone on github.com ...");
+                processor(
+                    "redirecting to https://github.com/ShKev03/WhatsChat..."
+                );
+                window
+                    .open("https://github.com/ShKev03/WhatsChat", "_blank")
+                    .focus();
+            } else {
+                processor("Directory not found: ./Whatsapp-Clone");
+            }
+            break;
+
+        case "cd projects/whatsapp-clone":
+        case "cd /projects/whatsapp-clone":
+        case "cd ./projects/whatsapp-clone":
+        case "cd projects/whatsapp-clone/":
+        case "cd /projects/whatsapp-clone/":
+        case "cd ./projects/whatsapp-clone/":
+            processor("opening /Whatsapp-Clone on github.com ...");
+            processor(
+                "redirecting to https://github.com/ShKev03/WhatsChat ..."
+            );
+            window
+                .open("https://github.com/ShKev03/WhatsChat", "_blank")
+                .focus();
+            break;
+
+        case "cd /instagram-clone":
+        case "cd instagram-clone":
+        case "cd ./instagram-clone":
+        case "cd /instagram-clone/":
+        case "cd instagram-clone/":
+        case "cd ./instagram-clone/":
+            if (directory == "/projects") {
+                processor("opening /Instagram-Clone on github.com ...");
+                processor(
+                    "redirecting to https://github.com/ShKev03/Instagram..."
+                );
+                window
+                    .open("https://github.com/ShKev03/Instagram", "_blank")
+                    .focus();
+            } else {
+                processor("Directory not found: ./Instagram-Clone");
+            }
+            break;
+
+        case "cd projects/instagram-clone":
+        case "cd /projects/instagram-clone":
+        case "cd ./projects/instagram-clone":
+        case "cd projects/instagram-clone/":
+        case "cd /projects/instagram-clone/":
+        case "cd ./projects/instagram-clone/":
+            processor("opening /Instagram-Clone on github.com ...");
+            processor(
+                "redirecting to https://github.com/ShKev03/Instagram ..."
+            );
+            // add a wait function and then redirect to github function here
+            processor("current directory :" + "/projects");
+            directory = "/projects";
+            window
+                .open("https://github.com/ShKev03/Instagram", "_blank")
+                .focus();
+            break;
+
+        case "cd /facebook-clone":
+        case "cd facebook-clone":
+        case "cd ./facebook-clone":
+        case "cd /facebook-clone/":
+        case "cd facebook-clone/":
+        case "cd ./facebook-clone/":
+            if (directory == "/projects") {
+                processor("opening /Facebook-Clone on github.com ...");
+                processor(
+                    "redirecting to https://github.com/ShKev03/Facebook..."
+                );
+                window
+                    .open("https://github.com/ShKev03/Facebook", "_blank")
+                    .focus();
+            } else {
+                processor("Directory not found: ./Facebook-Clone");
+            }
+            break;
+
+        case "cd projects/facebook-clone":
+        case "cd /projects/facebook-clone":
+        case "cd ./projects/facebook-clone":
+        case "cd projects/facebook-clone/":
+        case "cd /projects/facebook-clone/":
+        case "cd ./projects/facebook-clone/":
+            processor("opening /Facebook-Clone on github.com ...");
+            processor("redirecting to https://github.com/ShKev03/Facebook ...");
+            window
+                .open("https://github.com/shkev03/Facebook", "_blank")
+                .focus();
+            break;
+
+        case "insta":
+        case "instagram":
+            processor("opening Instagram on web browser ...");
+            window.open("https://instagram.com/shkev03", "_blank").focus();
+            break;
+
+        case "gmail":
+            processor("opening Gmail on web browser ...");
+            window
+                .open(
+                    "https://mail.google.com/mail/?view=cm&fs=1&to=keval.jignesh.shah@gmail",
+                    "_blank"
+                )
+                .focus();
+            break;
+
+        case "mail":
+        case "email":
+            processor("opening default mail client ...");
+            window
+                .open("mailto:keval.jignesh.shah@gmail.com", "_blank")
+                .focus();
+
+            break;
+
+        case "github":
+            processor("opening GitHub on web browser ...");
+            window.open("https://github.com/shkev03", "_blank").focus();
+            break;
+
+        case "x":
+        case "x.com":
+            processor("opening X.com on web browser ...");
+            window.open("https://x.com/shkev03", "_blank").focus();
+            break;
+
+        case "cat read.me":
+        case "cat /read.me":
+        case "cat ./read.me":
+            if (directory == "/") {
+                processor(
+                    "<div class='comment'>opening Readme.md file ...</div>"
+                );
+                processor(introduction);
+            } else {
+                processor("File not found: read.md");
+            }
+            break;
+
+        case "cat ../read.me":
+            if (directory == "/projects"){
+                processor(introduction)
+            } else {
+                processor("File not found: read.me")
+            }
             break
+
         default:
-            output.innerHTML += `<p>Command not found.</p><p>Please enter a valid command or enter <b>'help'</b> command to check out the commands available.</p>`;
+            processor(invalid);
             break;
     }
-    command.value = "";
+}
+
+function processor(out) {
+    output.innerHTML += `<p>${out}</p>`;
 }
